f950ff1b388a9995ffbb69ff52690c2607c43a85
saidu2 <safiyanusaidu2@gmail.com>
2018-06-29 14:34:34 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", fetchCurrencies)
+
+let currencyFrom = document.getElementById('currencyFrom')
+let currencyTo = document.getElementById('currencyTo')
+
+document.getElementById('button').addEventListener('click', convertCurrency)
+function fetchCurrencies() {
+    const  url = 'https://free.currencyconverterapi.com/api/v5/currencies'
+    fetch (url).then((response) => {
+        return response.json();
+    }).then(data=>{
+        let currencies = data.results
+        console.log(data);
+        upadateSelectbox(currencies)
+    });
+}
+
+function upadateSelectbox(currencies) {
+    for (currency in currencies) {
+        let optionFrom = document.createElement('option');
+        let optionTo = document.createElement('option')
+        optionTo.text = currency
+        optionTo.value = currency
+        currencyTo.appendChild(optionTo)
+
+
+        optionFrom.value = currency
+        optionFrom.text =  currency
+        currencyFrom.appendChild(optionFrom)
+    }
+}
+
+function convertCurrency(amount=0, fromCurrency, toCurrency) {
+    amount = document.getElementById('amount').value
+    fromCurrency = (currencyFrom.options[currencyFrom.selectedIndex].value)
+    toCurrency = (currencyTo.options[currencyTo.selectedIndex].value)
+    query = `${fromCurrency}_${toCurrency}`
+    let urlc = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`
+    fetch(urlc).then((response)=>{
+        return response.json()
+
+    }).then((data)=> {
+        console.log(data)
+        let val = data[query]
+        if (val){
+            let total = val * amount
+            document.getElementById('output').value = total.toFixed(2);
+        }else {
+            let err = new Error ('value not found'+ query)
+            console.log(err)
+        }
+    });
+}
+
+navigator.serviceWorker.register('service-worker.js').the(()=>{
+    console.log('[serviceWorker] Registered Succesfully!')
+}).catch(()=> {
+    console.log('[serviceWorker] Registration Failed', )
+})
---
+++ service-worker.js
@@ -0,0 +1,57 @@
+let cacheName = 'v3';
+let contentToCache = [
+    './',
+    './index.html',
+    './main.js',
+    './CSS/style.css',
+];
+
+self.addEventListener('install', (event)=> {
+    console.log("[serviceWorker] installed")
+    event.waitUntil(
+        caches.open(cacheName).then((cache)=> {
+            console.log("[serviceWorker] caching files");
+            return cache.addAll([contentToCache]);
+        })
+    );
+});
+
+self.addEventListener('activated', (event)=> {
+    console.log("[serviceWorker] activated")
+    event.waitUntil(
+        caches.keys().then((cacheNames)=>{
+            return Promise.all(cacheNames.map((thisCacheName)=>{
+                if (thisCacheName !== cachName)
+                console.log("[serviceWorker] removing cache from ", thisCacheName)
+                return caches.delete(thisCacheName);
+            }))
+        })
+    )
+});
+
+self.addEventListener('fetch', (event)=> {
+    console.log("[serviceWorker] fetching ");
+    event.respondWith(
+        caches.match(event.request).then((response)=> {
+            if ( response) {
+                console.log("[serviceWorker] Found in cache");
+                return response;
+            }
+            let requestClone = event.request.clone();
+
+            fetch(requestClone).then((response)=>{
+              if (!response) {
+                  console.log("[serviceWorker] no response from fetch")
+                  return response;
+              }
+              let responseClone =  response.clone();
+              caches.open(cacheName).then((cache)=>{
+                 cache.put(event.request, event.responseClone);
+                 return response;
+              });
+          }).catch((err)=>{
+              console.log("[serviceWorker] Error Fetching and Caching New Data", err);
+          })
+      }));
+
+})
